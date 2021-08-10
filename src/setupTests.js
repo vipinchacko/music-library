e2d996a7c5c4af4ad25300405f7218dfe8fdf42d
vipin7 <vipinchackomdy@gmail.com>
2021-08-10 12:03:29 +0530

    Add mock for browser session storage apis

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,30 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const sessionStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key] || null;
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+beforeEach(() => {
+  sessionStorageMock.clear();
+});
+
+Object.defineProperty(window, 'sessionStorage', {
+  value: sessionStorageMock,
+});
