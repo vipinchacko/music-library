ad8e7c2c15d877ecd114282e4854c67772538c1b
vipin7 <vipinchackomdy@gmail.com>
2021-08-10 11:58:36 +0530

    Show additional details of album

--- src/components/AlbumCard/AlbumInfo/index.js
+++ src/components/AlbumCard/AlbumInfo/index.js
@@ -1,10 +1,64 @@
 import styles from './AlbumInfo.module.scss';
 
-const AlbumInfo = ({ album, artist }) => {
+import favouriteIcon from '../../../static/icons/favourite.png';
+import addToFavouriteIcon from '../../../static/icons/addToFavourite.png';
+import { Fragment } from 'react';
+
+const AlbumInfo = ({
+  id,
+  album,
+  artist,
+  price,
+  genre,
+  songCount,
+  showDetails,
+  isFavourite,
+  hideFavouriteButton,
+  onAddToFavourites,
+}) => {
   return (
-    <div className={styles.albumInfo}>
-      <span className={styles.album}>{album}</span>
-      <span className={styles.artist}>{artist}</span>
+    <div className={styles.albumInfoParent}>
+      <div className={styles.albumInfo}>
+        <span className={styles.album} data-testid="album">
+          {album}
+        </span>
+        <span className={styles.artist} data-testid="artist">
+          {artist}
+        </span>
+        <div
+          className={`${styles.details} ${
+            showDetails ? styles.detailsExpand : ''
+          }`}
+        >
+          {showDetails && (
+            <Fragment>
+              <span className={styles.price} data-testid="price">
+                {price}
+              </span>
+              <span className={styles.genre} data-testid="genre">
+                {genre}
+              </span>
+              <span className={styles.songCount} data-testid="songCount">
+                {songCount} Songs
+              </span>
+            </Fragment>
+          )}
+        </div>
+      </div>
+      <button
+        value={id}
+        className={styles.favouriteButton}
+        onClick={onAddToFavourites}
+      >
+        {!hideFavouriteButton && (
+          <img
+            className={styles.favouriteIcon}
+            src={isFavourite ? favouriteIcon : addToFavouriteIcon}
+            alt="favouriteIcon"
+            data-testid={isFavourite ? 'favourite' : 'addToFavourite'}
+          />
+        )}
+      </button>
     </div>
   );
 };
